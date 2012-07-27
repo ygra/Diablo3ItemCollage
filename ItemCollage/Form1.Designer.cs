b749bbf04ebf289c36ad9e1f2260b2b1e5131bab
Ygra <github@hypftier.de>
2012-07-28 01:21:17 +0200

    Initial commit.

---
+++ ItemCollage/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ItemCollage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "grab! (F1)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 278);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(191, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "collage";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Item Collage";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ItemCollage/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ItemCollage
+{
+    public partial class Form1 : Form
+    {
+        GlobalHotkey F1;
+        List<Image> items = new List<Image>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            F1 = new GlobalHotkey(Constants.NOMOD, Keys.F1, this);
+            F1.Register();
+            UpdateLabel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HandleF1();
+        }
+
+        private Rectangle FindFrame(Bitmap bmp, Point p)
+        {
+            try
+            {
+                var extentUp = p.Y -
+                    Enumerable.Range(0, p.Y)
+                        .TakeWhile(y => bmp.GetPixel(p.X, p.Y - y).ToArgb() == Color.Black.ToArgb())
+                        .Last();
+                var extentDown =
+                    Enumerable.Range(p.Y, bmp.Height - p.Y)
+                        .TakeWhile(y => bmp.GetPixel(p.X, y).ToArgb() == Color.Black.ToArgb())
+                        .Last();
+                var extentLeft = p.X -
+                    Enumerable.Range(0, p.X)
+                        .TakeWhile(x => bmp.GetPixel(p.X - x, extentUp).ToArgb() == Color.Black.ToArgb() && bmp.GetPixel(p.X - x, extentDown).ToArgb() == Color.Black.ToArgb())
+                        .Last();
+                var extentRight =
+                    Enumerable.Range(p.X, bmp.Width)
+                        .TakeWhile(x => bmp.GetPixel(x, extentUp).ToArgb() == Color.Black.ToArgb() && bmp.GetPixel(x, extentDown).ToArgb() == Color.Black.ToArgb())
+                        .Last();
+                return new Rectangle(extentLeft, extentUp, extentRight - extentLeft, extentDown - extentUp);
+            }
+            catch
+            {
+                return new Rectangle();
+            }
+        }
+
+        private IEnumerable<int> Range(int start, int end, int step = 1)
+        {
+            int i;
+            for (i = start; i < end; i += step)
+            {
+                yield return i;
+            }
+            if (i < end) yield return end;
+        }
+
+        private void HandleF1()
+        {
+            try
+            {
+                var screen = TakeScreenshot();
+                //pictureBox1.Image = screen;
+                var cursorPos = Cursor.Position;
+
+                Rectangle bounds;
+
+                if (cursorPos.X < screen.Width / 2)
+                {
+                    var blockSize = new Size(3, 8);
+                    var searchRect = new Rectangle(cursorPos.X - 20, cursorPos.Y - 210, 180, 350);
+                    // find block of black pixels
+                    var blocks = from y in Range(searchRect.Top, searchRect.Bottom, 8)
+                                 from x in Range(searchRect.Left, searchRect.Right)
+                                 where (from yy in Range(0, blockSize.Height, 3)
+                                        from xx in Range(0, blockSize.Width)
+                                        select screen.GetPixel(x + xx, y + yy)).All(c => c.ToArgb() == Color.Black.ToArgb())
+                                 select new Point(x, y);
+                    var leftEdge = (from b in blocks orderby b.X select b).First();
+                    var topEdge =
+                        Enumerable.Range(0, leftEdge.Y)
+                            .Reverse()
+                            .TakeWhile(y => screen.GetPixel(leftEdge.X, y).ToArgb() == Color.Black.ToArgb())
+                            .Last();
+                    var topLeftCorner = new Point(leftEdge.X, topEdge);
+                    var width =
+                        Enumerable.Range(topLeftCorner.X, 650)
+                            .TakeWhile(x => screen.GetPixel(x, topLeftCorner.Y).ToArgb() == Color.Black.ToArgb())
+                            .Count();
+                    var height =
+                        Enumerable.Range(topLeftCorner.Y, screen.Height - topLeftCorner.Y)
+                            .TakeWhile(y => screen.GetPixel(topLeftCorner.X, y).ToArgb() == Color.Black.ToArgb())
+                            .Count();
+                    bounds = new Rectangle(topLeftCorner, new Size(width, height));
+                }
+                else
+                {
+                    var blockSize = new Size(2, 30);
+                    var searchRect = new Rectangle(cursorPos.X - 160, cursorPos.Y - 210, 160, 210);
+                    // find block of black pixels
+                    var blocks = from y in Range(searchRect.Top, searchRect.Bottom, 4)
+                                 from x in Range(searchRect.Left, searchRect.Right)
+                                 where (from yy in Range(0, blockSize.Height, 5)
+                                        from xx in Range(0, blockSize.Width)
+                                        select screen.GetPixel(x - xx, y - yy)).All(c => c.ToArgb() == Color.Black.ToArgb())
+                                 select new Point(x, y);
+                    var rightEdge = (from b in blocks orderby b.X select b).Last();
+                    var topEdge =
+                        Enumerable.Range(0, rightEdge.Y)
+                            .Reverse()
+                            .TakeWhile(y => screen.GetPixel(rightEdge.X, y).ToArgb() == Color.Black.ToArgb())
+                            .Last();
+                    var topRightCorner = new Point(rightEdge.X, topEdge);
+                    var width =
+                        Enumerable.Range(0, topRightCorner.X)
+                            .TakeWhile(x => screen.GetPixel(topRightCorner.X - x, topRightCorner.Y).ToArgb() == Color.Black.ToArgb())
+                            .Count();
+                    var height =
+                        Enumerable.Range(topRightCorner.Y, screen.Height - topRightCorner.Y)
+                            .TakeWhile(y => screen.GetPixel(topRightCorner.X, y).ToArgb() == Color.Black.ToArgb())
+                            .Count();
+                    bounds = new Rectangle(topRightCorner.X - width + 1, topRightCorner.Y, width, height);
+                }
+
+                if (bounds.Width < 100) return;
+
+                Bitmap item = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format24bppRgb);
+                Graphics g = Graphics.FromImage(item);
+                g.DrawImage(screen, new Rectangle(0, 0, bounds.Width, bounds.Height), bounds, GraphicsUnit.Pixel);
+                pictureBox1.Image = item;
+                items.Add(item);
+                UpdateLabel();
+            }
+            catch { }
+        }
+
+        private void UpdateLabel()
+        {
+            label1.Text = string.Format("{0} item(s) copied.", items.Count);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                HandleF1();
+            base.WndProc(ref m);
+        }
+
+        private Bitmap TakeScreenshot()
+        {
+            var bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap bmp = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format24bppRgb);
+            var g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(bounds.X, bounds.Y, 0, 0, new Size(bounds.Width, bounds.Height));
+            return bmp;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            F1.Unregister();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (items.Count == 0) return;
+            items.RemoveAt(items.Count - 1);
+            if (items.Count > 0)
+                pictureBox1.Image = items[items.Count - 1];
+            else pictureBox1.Image = null;
+
+            UpdateLabel();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (items.Count == 0) return;
+
+                //int numCols = items.Count > 12 ? 4 : 3;
+                int numCols = (int)Math.Min(Math.Ceiling(Math.Sqrt(items.Count)), 4); // Ventero
+                int w = items[0].Width;
+                var colLengths = new int[numCols];
+
+                foreach (var item in items)
+                {
+                    var col = Range(0, numCols).Select(i => new { i = i, h = colLengths[i] }).OrderBy(k => k.h).First().i;
+                    colLengths[col] += item.Height;
+                }
+
+                Bitmap b = new Bitmap(numCols * w, colLengths.Max(), PixelFormat.Format16bppRgb555);
+                Graphics g = Graphics.FromImage(b);
+                //g.FillRectangle(Brushes.Black, 0, 0, b.Width, b.Height);
+                colLengths = new int[numCols];
+
+                int itemIndex = 1;
+
+                foreach (var item in items)
+                {
+                    var col = Range(0, numCols).Select(i => new { i = i, h = colLengths[i] }).OrderBy(k => k.h).First().i;
+                    g.DrawImageUnscaledAndClipped(item, new Rectangle(w * col, colLengths[col], w, item.Height));
+                    g.DrawString(itemIndex.ToString(), new Font("Arial", 20, FontStyle.Bold), Brushes.White, col * w + 10, colLengths[col] + 10);
+                    colLengths[col] += item.Height;
+                    itemIndex++;
+                }
+
+                var picFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                var fileName = string.Format("items-{0:yyyy-MM-dd-HH-mm-ss}.png", DateTime.UtcNow);
+                var file = Path.Combine(picFolder, fileName);
+                b.Save(file);
+                items.Clear();
+                UpdateLabel();
+            }
+            catch { }
+        }
+    }
+}
