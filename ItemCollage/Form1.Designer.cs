7be7caaa9dcff219da580c5f1ba4baa80c4e4ce4
Johannes Rössel <github@hypftier.de>
2012-08-03 21:50:54 +0200

    Redone item extraction, doesn't work yet, though
    Goal should be reliable extraction at all resolutions as well as at
    every point on the screen.

--- ItemCollage/Form1.Designer.cs
+++ ItemCollage/Form1.Designer.cs
@@ -54,7 +54,6 @@
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(253, 278);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
