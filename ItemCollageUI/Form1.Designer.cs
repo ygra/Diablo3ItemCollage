4d8896ee30b3a556e7520bd02844ff0ce8f268ad
Ygra <github@hypftier.de>
2012-08-16 01:28:44 +0200

    UI redesigned a bit

--- ItemCollageUI/Form1.Designer.cs
+++ ItemCollageUI/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.itemListBox1 = new ItemCollage.ItemListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,7 +41,7 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 2;
@@ -50,25 +51,26 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 306);
+            this.panel1.Location = new System.Drawing.Point(0, 288);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(249, 31);
+            this.panel1.Size = new System.Drawing.Size(264, 49);
             this.panel1.TabIndex = 3;
             // 
             // label2
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.ForeColor = System.Drawing.Color.Silver;
-            this.label2.Location = new System.Drawing.Point(124, 9);
+            this.label2.Location = new System.Drawing.Point(3, 3);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.Size = new System.Drawing.Size(258, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Grab: F1       Collage: F2";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Text = "Grab items with F1, click them to remove";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label2.UseMnemonic = false;
             // 
             // itemListBox1
@@ -80,15 +82,25 @@
             this.itemListBox1.ForeColor = System.Drawing.Color.White;
             this.itemListBox1.Location = new System.Drawing.Point(0, 0);
             this.itemListBox1.Name = "itemListBox1";
-            this.itemListBox1.Size = new System.Drawing.Size(249, 306);
+            this.itemListBox1.Size = new System.Drawing.Size(264, 288);
             this.itemListBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(186, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(249, 337);
+            this.ClientSize = new System.Drawing.Size(264, 337);
             this.Controls.Add(this.itemListBox1);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
@@ -113,6 +125,7 @@
         private System.Windows.Forms.Panel panel1;
         private ItemListBox itemListBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
