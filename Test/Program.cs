90ee04e6b2f6f43500fdd4bdee1ffd287bd18ce5
Ventero <ventero@ventero.de>
2012-09-02 22:38:11 +0200

    Parallelize test suite
    For some reason, the item extraction slows down by 33% when running with
    4 threads though, so we don't quite get an ideal speedup.
    
    Refs #11

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,25 +1,73 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Test
 {
+    class ConsoleOutput
+    {
+        public string Text { get; set; }
+        public ConsoleColor? Color { get; set; }
+    }
+
+    class ConsoleOutputList : List<ConsoleOutput>
+    {
+        public void Add(string text, ConsoleColor? color = null)
+        {
+            this.Add(new ConsoleOutput
+            {
+                Text = text,
+                Color = color
+            });
+        }
+
+        public void AddTime(double time)
+        {
+            ConsoleColor? color = null;
+            if (time > 0.2) color = ConsoleColor.Red;
+            else if (time > 0.1) color = ConsoleColor.Yellow;
+
+            this.Add(string.Format("{0:F3}s", time), color);
+        }
+
+        public void Print()
+        {
+            foreach (var output in this)
+            {
+                if (output.Color.HasValue) Console.ForegroundColor = output.Color.Value;
+                else Console.ResetColor();
+                Console.Write(output.Text);
+            }
+            Console.WriteLine();
+
+            this.Clear();
+        }
+    }
+
     class Program
     {
         static Regex cursorPattern = new Regex(@"P(\d+)-(\d+)");
         static string folderPath = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
             "ExtractTest");
-        static double extractTime = 0.0;
-        static double extractTitleTime = 0.0;
+
+        static ConcurrentBag<double> extractTimes = new ConcurrentBag<double>();
+        static ConcurrentBag<double> extractTitleTimes = new ConcurrentBag<double>();
+
+        static int test = 1;
+        static int success = 0;
+        static int numTests = 0;
+
+        static object lockobj = new object();
 
         static void Main(string[] args)
         {
@@ -31,43 +79,57 @@ namespace Test
 
             var folder = new DirectoryInfo(folderPath);
             var files = folder.GetFiles("*.in.png");
-            var numTests = files.Count();
-            int test = 0, success = 0;
+            numTests = files.Count();
 
             var sw = new Stopwatch();
             sw.Start();
-
-            foreach (var input in files)
-            {
-                var infile = input.FullName;
-                var outfile = infile.Replace(".in.png", ".out.png");
-                var titlefile = infile.Replace(".in.png", ".title.png");
-
-                Console.ResetColor();
-                Console.Write(string.Format("Test {0}/{1}... ", ++test, numTests));
-
-                string reason = CompareItemExtraction(infile, outfile, titlefile);
-                if (reason == "")
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("passed!");
-                    success++;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("failed ({0})", reason);
-                    Console.WriteLine(input.Name);
-                }
-            }
-
+            Parallel.ForEach(files, TestFile);
             sw.Stop();
 
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ResetColor();
             Console.WriteLine("{0} of {1} tests succeeded.", success, numTests);
             Console.WriteLine("Time taken: {0} total", sw.Elapsed);
-            Console.WriteLine("Extraction: {0:F3}s - Titles: {1:F3}s", extractTime,
-                extractTitleTime);
+            Console.WriteLine("Extraction: {0:F3}s - Titles: {1:F3}s", extractTimes.Sum(),
+                extractTitleTimes.Sum());
+            Console.WriteLine("Slow extractions: {0} / {1} - {2} / {3}",
+                extractTimes.Where(t => t > 0.1 && t <= 0.2).Count(),
+                extractTimes.Where(t => t > 0.2).Count(),
+                extractTitleTimes.Where(t => t > 0.1 && t <= 0.2).Count(),
+                extractTitleTimes.Where(t => t > 0.2).Count());
+        }
+
+        private static void TestFile(FileInfo input)
+        {
+            var infile = input.FullName;
+            var outfile = infile.Replace(".in.png", ".out.png");
+            var titlefile = infile.Replace(".in.png", ".title.png");
+
+            Console.ResetColor();
+            var output = new ConsoleOutputList();
+            output.Add(string.Format("Test {0}/{1}... ", test, numTests));
+            Interlocked.Increment(ref test);
+
+            string reason = CompareItemExtraction(infile, outfile, titlefile, output);
+            if (reason == "")
+            {
+                output.Add("passed!", ConsoleColor.Green);
+                Interlocked.Increment(ref success);
+            }
+            else
+            {
+                output.Add(string.Format("failed ({0})\n{1}", reason, input.Name),
+                    ConsoleColor.Red);
+            }
+
+            PrintOutput(output);
+        }
+
+        private static void PrintOutput(ConsoleOutputList outlist)
+        {
+            lock (lockobj)
+            {
+                outlist.Print();
+            }
         }
 
         private static string CompareImageResult(Bitmap bmp, string file, string type)
@@ -113,9 +175,9 @@ namespace Test
                     byte* expectedStart = (byte*)expectedData.Scan0;
                     for (var y = 0; y < resultData.Height; y++)
                     {
-                        var resultRow = (IntPtr)(resultStart + y*resultData.Stride);
-                        var expectedRow = (IntPtr)(expectedStart + y*expectedData.Stride);
-                        if (memcmp(resultRow, expectedRow, resultData.Width*3) != 0)
+                        var resultRow = (IntPtr)(resultStart + y * resultData.Stride);
+                        var expectedRow = (IntPtr)(expectedStart + y * expectedData.Stride);
+                        if (memcmp(resultRow, expectedRow, resultData.Width * 3) != 0)
                         {
                             return string.Format("{0} bitmap data did not match", type);
                         }
@@ -132,7 +194,7 @@ namespace Test
         }
 
         private static string CompareItemExtraction(string infile, string outfile,
-            string titlefile)
+            string titlefile, ConsoleOutputList output)
         {
             var match = cursorPattern.Match(infile);
             if (!match.Success)
@@ -146,21 +208,20 @@ namespace Test
                 Convert.ToInt32(match.Groups[2].Captures[0].Value));
 
             var sw = new Stopwatch();
-            sw.Start();
             var ie = new ItemCollage.ItemExtractor(bmp, cursorPos);
             Bitmap item = null;
+            sw.Start();
             try
             {
                 item = (Bitmap)ie.ExtractItem();
             }
             catch { }
-
             sw.Stop();
 
             var itemTime = sw.Elapsed.TotalSeconds;
-            extractTime += itemTime;
-            Console.Write("(");
-            WriteTime(itemTime);
+            extractTimes.Add(itemTime);
+            output.Add("(");
+            output.AddTime(itemTime);
 
             var result = CompareImageResult(item, outfile, "item");
             if (result == "")
@@ -168,33 +229,26 @@ namespace Test
                 Bitmap title = null;
                 if (item != null)
                 {
-                    sw.Reset();
-                    sw.Start();
+                    var titleWatch = new Stopwatch();
+                    titleWatch.Start();
                     title = ItemCollage.ItemExtractor.ExtractItemName(item, true);
-                    sw.Stop();
+                    titleWatch.Stop();
 
-                    var titleTime = sw.Elapsed.TotalSeconds;
-                    extractTitleTime += titleTime;
-                    Console.Write(" - ");
-                    WriteTime(titleTime);
+                    var titleTime = titleWatch.Elapsed.TotalSeconds;
+                    extractTitleTimes.Add(titleTime);
+                    output.Add(" - ");
+                    output.AddTime(titleTime);
 
                 }
 
                 result = CompareImageResult(title, titlefile, "title");
             }
 
-            Console.Write(") ");
+            output.Add(") ");
+
             return result;
         }
 
-        private static void WriteTime(double time)
-        {
-            if (time > 0.1) Console.ForegroundColor = ConsoleColor.Yellow;
-            else if (time > 0.2) Console.ForegroundColor = ConsoleColor.Red;
-
-            Console.Write("{0:F3}s", time);
-            Console.ResetColor();
-        }
 
         [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern int memcmp(IntPtr b1, IntPtr b2, long count);
