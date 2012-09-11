b2f0aee7535c22168d3c5483b2da653e4bcd3265
Ygra <github@hypftier.de>
2012-09-11 19:22:15 +0200

    Added a minimum size for the window so our auto-adjusting width doesn't create an unusable mess on smaller resolutions

--- ItemCollageUI/Form1.Designer.cs
+++ ItemCollageUI/Form1.Designer.cs
@@ -131,6 +131,7 @@
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.White;
             this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(250, 125);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
