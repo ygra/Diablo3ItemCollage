00d3e4694a74d3ace7426bf55a1ab22b526b83eb
Johannes Rössel <github@hypftier.de>
2021-09-27 23:30:18 +0200

    Initial attempt to port to .NET 5

--- ItemCollageUI/Program.cs
+++ ItemCollageUI/Program.cs
@@ -16,17 +16,17 @@ namespace ItemCollage
         {
             // Technique for loading DLLs from resources in the application
             // Taken from http://blogs.msdn.com/b/microsoft_press/archive/2010/02/03/jeffrey-richter-excerpt-2-from-clr-via-c-third-edition.aspx
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-            {
-                String resourceName = "ItemCollage.lib." +
-                   new AssemblyName(args.Name).Name + ".dll";
-                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
-                {
-                    Byte[] assemblyData = new Byte[stream.Length];
-                    stream.Read(assemblyData, 0, assemblyData.Length);
-                    return Assembly.Load(assemblyData);
-                }
-            };
+//            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+  //          {
+    //            String resourceName = "ItemCollage.lib." +
+        //           new AssemblyName(args.Name).Name + ".dll";
+      //          using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+          //      {
+              //      Byte[] assemblyData = new Byte[stream.Length];
+            //        stream.Read(assemblyData, 0, assemblyData.Length);
+//                    return Assembly.Load(assemblyData);
+  //              }
+    //        };
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
